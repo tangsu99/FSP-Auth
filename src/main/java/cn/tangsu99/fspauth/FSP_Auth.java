975347173d658d7a305acd0036940db7e548dfb1
tangsu99 <18763952906@163.com>
2025-01-31 10:45:55 +0800

    Initial

---
+++ src/main/java/cn/tangsu99/fspauth/FSP_Auth.java
@@ -0,0 +1,22 @@
+package cn.tangsu99.fspauth;
+
+import com.google.inject.Inject;
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
+import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.plugin.Plugin;
+import org.slf4j.Logger;
+
+@Plugin(
+        id = "fsp-auth",
+        name = "FSP-Auth",
+        version = BuildConstants.VERSION
+)
+public class FSP_Auth {
+
+    @Inject
+    private Logger logger;
+
+    @Subscribe
+    public void onProxyInitialization(ProxyInitializeEvent event) {
+    }
+}
---
+++ src/main/templates/cn/tangsu99/fspauth/BuildConstants.java
@@ -0,0 +1,7 @@
+package cn.tangsu99.fspauth;
+
+// The constants are replaced before compilation
+public class BuildConstants {
+
+    public static final String VERSION = "${version}";
+}
